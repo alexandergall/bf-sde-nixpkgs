f85afb4748b5e615a85e91b551059a843ed9dc8b
Alexander Gall <alexander.gall@gmail.com>
2020-09-16 12:31:34 +0200

    Change kernel definitions from a list to an attribute set.

--- overlays/bf-sde/kernels/default.nix
+++ overlays/bf-sde/kernels/default.nix
@@ -24,19 +24,17 @@ let
           config = { CONFIG_MODULES = "y"; };
         };
 
-in builtins.foldl' (res: attrs: res // { "${attrs.id}" = mkKernel attrs; }) {} [
+in lib.mapAttrs (id: spec: mkKernel { inherit id; inherit (spec) version localVersion sha256; }) {
   ## sha256 hashes can be determined with
   ## nix-prefetch-url  http://cdn.kernel.org/pub/linux/kernel/v4.x/linux-<kernelVersion>.tar.xz 
-  rec {
-    id = "k4_14_151_ONL_7c3bfd";
+  k4_14_151_ONL_7c3bfd = {
     version = "4.14.151";
     localVersion = "-OpenNetworkLinux";
     sha256 = "1bizb1wwni5r4m5i0mrsqbc5qw73lwrfrdadm09vbfz9ir19qlgz";
-  }
-  rec {
-    id = "k4_19_81_ONL_1537d8";
+  };
+  k4_19_81_ONL_1537d8 = {
     version = "4.19.81";
     localVersion = "-OpenNetworkLinux";
     sha256 = "17g2wiaa7l7mxi72k79drxij2zqk3nsj8wi17bl4nfvb1ypc2gi9";
-  }
-]
+  };
+}
