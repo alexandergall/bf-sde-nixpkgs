9104660c878f187a2d8d59ead25b9aa21be24d70
Alexander Gall <alexander.gall@gmail.com>
2020-10-19 13:33:44 +0200

    build_p4_program.nix: add execName, kernelModule, remove symlinks
    The new option execName allows building the program under a name
    that differs from p4Name. This allows adding variants of the same
    P4 program to an environment without causing conflicts.
    
    For the same reason, the symlinks to commands in the SDE package are
    removed.
    
    The new option kernelModule loads the requested kernel module before
    starting bf_switchd.

--- bf-sde/build-p4-program.nix
+++ bf-sde/build-p4-program.nix
@@ -1,23 +1,31 @@
 { stdenv, lib, bf-sde, getopt, which, coreutils, findutils,
-  procps, gnugrep, gnused, utillinux, runtimeShell }:
+  procps, gnugrep, gnused, gawk, kmod, utillinux, runtimeShell }:
 
-{ name ? attrs.${name},
-  version ? attrs.${version},
+{ name,
+  version,
 # The name of the p4 program to compile without the ".p4" extension
-  p4Name ? attrs.${p4Name},
+  p4Name,
+# The name to use for the generated script that starts bf_switchd
+# with the program
+  execName ? p4Name,
 # The path to the program relative to the root of the source tree.
 # I.e. the program to be compiled si expected to be
 #   <path>/<p4Name>.p4
-  path ? null,
+  path ? ".",
+# The kernel module to load before launching bf_switchd. Must be one
+# of bf_kdrv, bf_kpkt, bf_knet
+  kernelModule ? null,
 # The flags passed to the p4_build.sh script
   buildFlags ? "",
-  src ? attrs.${src},
-} @attrs:
+  src
+}:
+
+assert kernelModule != null -> lib.any (e: kernelModule == e) [ "bf_kdrv" "bf_kpkt" "bf_knet" ];
 
 stdenv.mkDerivation rec {
   buildInputs = [ bf-sde getopt which procps ];
 
-  inherit (attrs) name version src p4Name;
+  inherit name version src p4Name;
   
   buildPhase = ''
     set -e
@@ -27,7 +35,13 @@ stdenv.mkDerivation rec {
     export SDE_BUILD=$TEMP
     export SDE_LOGS=$TEMP
     mkdir $out
-    cmd="${bf-sde}/bin/p4_build.sh ${buildFlags} ${if path == null then "." else path}/${p4Name}.p4"
+    path=${path}
+    exec_name="${p4Name}"
+    if [ "${p4Name}" != "${execName}" ]; then
+      ln -s ${p4Name}.p4 $path/${execName}.p4
+      exec_name=${execName}
+    fi
+    cmd="${bf-sde}/bin/p4_build.sh ${buildFlags} $path/$exec_name.p4"
     echo "Build command: $cmd"
     $cmd
   '';
@@ -35,7 +49,7 @@ stdenv.mkDerivation rec {
   installPhase = ''
 
     ## This script will execute bf_switchd with our P4 program
-    command=$out/bin/${p4Name}
+    command=$out/bin/$exec_name
     mkdir $out/bin
     cat <<EOF > $command
     #!${runtimeShell}
@@ -46,20 +60,29 @@ stdenv.mkDerivation rec {
     ## We would like to make this self-contained with nixpkgs, but
     ## sudo is a special case because suid executables are not
     ## supported. Hence we use sudo from /usr/bin for the time being.
-    export PATH=${lib.strings.makeBinPath [ coreutils findutils gnugrep gnused utillinux procps ]}:/usr/bin
-    ${bf-sde}/run_switchd.sh -p ${p4Name}
+    export PATH=${lib.strings.makeBinPath [ coreutils findutils gnugrep gnused gawk kmod utillinux procps ]}:/usr/bin
+  '' + (lib.optionalString (kernelModule != null)
+  ''
+
+    function mod_exists {
+      lsmod | awk '{print $1}' | grep \$1 >/dev/null
+    }
+
+    for mod in bf_kdrv bf_knet bf_kpkt; do
+      [ \''${mod} == ${kernelModule} ] && continue
+      if mod_exists \''${mod}; then
+        echo "Unloading \''${mod}"
+        sudo ${bf-sde}/bin/\''${mod}_mod_unload
+      fi
+    done
+    if ! mod_exists ${kernelModule}; then
+      echo "Loading ${kernelModule}"
+      sudo ${bf-sde}/bin/${kernelModule}_mod_load
+    fi
+  '') +
+  ''
+    ${bf-sde}/run_switchd.sh -p $exec_name
     EOF
     chmod a+x $command
-
-    ## Create links to bfshell and the kernel module load/unload scripts for convenience
-    ln -s ${bf-sde}/bin/bfshell $out/bin
-    for mod in kdrv knet kpkt; do
-      name=bf_''${mod}_mod
-      ln -s ${bf-sde}/bin/''${name}_unload $out/bin/''${name}_unload
-      load=$out/bin/''${name}_load
-      echo '#!${runtimeShell}' >>$load
-      echo "${bf-sde}/bin/''${name}_load ${bf-sde}" >>$load
-      chmod a+x $load
-    done
   '';
 }
