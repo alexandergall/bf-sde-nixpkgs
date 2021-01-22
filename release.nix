6114df1117ea6510b07b96b564879ac0bff95799
Alexander Gall <alexander.gall@gmail.com>
2021-01-22 17:37:25 +0100

    release.nix: improve coding style

--- release.nix
+++ release.nix
@@ -4,17 +4,18 @@
 
 let
   pkgs = import ./. {};
-in with pkgs;
+in
+with pkgs;
 with lib;
 
 let
   ## Hydra doesn't like non-derivation attributes
-  bf-sde = filterAttrs (n: v: attrsets.isDerivation v) pkgs.bf-sde;
+  bf-sde' = filterAttrs (n: v: attrsets.isDerivation v) bf-sde;
   kernels = import ./bf-sde/kernels pkgs;
   mk = sde: {
     inherit sde;
     inherit (sde) pkgs;
     kernelModules = mapAttrs (kernelID: _: sde.buildModules kernelID) kernels;
   };
-  versions = mapAttrs (version: sde: mk sde) bf-sde;
+  versions = mapAttrs (version: sde: mk sde) bf-sde';
 in versions
