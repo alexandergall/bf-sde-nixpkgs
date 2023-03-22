2b0d6e1964c093913a4c7a93f6189a8bac37f0a3
Alexander Gall <alexander.gall@gmail.com>
2023-03-22 17:22:17 +0100

    Prepare default.nix to receive nixpkgs as input

--- default.nix
+++ default.nix
@@ -1,10 +1,11 @@
-{ overlays ? [], ... } @attrs:
-
-let
-  nixpkgsSrc = fetchTarball {
+{ overlays ? []
+, nixpkgs ?
+  fetchTarball {
     url = https://github.com/NixOS/nixpkgs/archive/22.05-1567-g099cb1a04e5.tar.gz;
     sha256 = "0pfqqsw97bflm1yby8xy697q2lkh7gp66ggcdwnjd5z1xy1y44vv";
-  };
-in import nixpkgsSrc ( attrs // {
-  overlays = (import ./overlay.nix) nixpkgsSrc ++ overlays;
+  }
+,  ... } @attrs:
+
+import nixpkgs ( attrs // {
+  overlays = (import ./overlay.nix) nixpkgs ++ overlays;
 })
